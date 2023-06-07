35ecc50cf0322c4a6bc370c62393bbbe7148bc52
Onewaypie <Onewaypie@gmail.com>
2023-06-07 14:19:58 +0200

    REALLY BIG
    Added test level
    
    NEW assets for nature levels
    
    PC = exploded

--- Brackys/Assets/scripts/PlayerMovements/SkinsPlayer.cs
+++ Brackys/Assets/scripts/PlayerMovements/SkinsPlayer.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class SkinsPlayer : MonoBehaviour
 {
-    public GameObject yourGameObject;
-    MeshFilter yourMesh;
-    
+    //public GameObject yourGameObject;
+    //MeshFilter yourMesh;
+    public MeshRenderer meshrend;
+    public Material mat;
 
     // Start is called before the first frame update
     void Start()
     {
-         yourMesh = yourGameObject.GetComponent<MeshFilter>();
-    yourMesh.sharedMesh = Resources.Load<Mesh>("Test");
+        // yourMesh = yourGameObject.GetComponent<MeshFilter>();
+   // yourMesh.sharedMesh = Resources.Load<Mesh>("Test");
+        
+        meshrend.material = mat;
     }
 
     // Update is called once per frame
