22562d180c466bca24ee4e4750ca622b564d4f68
Onewaypie <Onewaypie@gmail.com>
2023-06-10 20:41:19 +0200

    nature level update

---
+++ Brackys/Assets/Scenes/TestLEVEL/falling.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class falling : MonoBehaviour
+{
+    private GameObject treethatfalls;
+    // Start is called before the first frame update
+    void Start()
+    {
+        treethatfalls = GetComponent<GameObject>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
