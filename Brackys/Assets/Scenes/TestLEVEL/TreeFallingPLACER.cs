ef7270143f170e09d33be2234d5c869441719472
Onewaypie <Onewaypie@gmail.com>
2023-06-10 23:48:20 +0200

    i fixed everything that was broken

---
+++ Brackys/Assets/Scenes/TestLEVEL/TreeFallingPLACER.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeFallingPLACER : MonoBehaviour
+{
+    public GameObject FallingTree;
+    private int amount;
+    private int x;
+    private float y;
+    private int z;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+
+        
+        
+        Destroy(this);
+        for(int i=0; i<100  ; i++)
+        {
+            x =  Random.Range( 100,  297);
+            y = -0.6F;
+            z =  Random.Range( 200,  855);
+            Instantiate(FallingTree, new Vector3(x,y,z), Quaternion.Euler(0,0,0));
+        }
+    }
+}
--- Brackys/Assets/Scenes/TestLEVEL/falling.cs
+++ Brackys/Assets/Scenes/TestLEVEL/falling.cs
@@ -2,39 +2,46 @@
 
 
 
+
+
 using UnityEngine;
 
 public class falling : MonoBehaviour
 {
     public GameObject FallingTree;
-    public float test;
-    private Collider area;
-    private int amount;
-    private int x;
-    private float y;
-    private int z;
+    private float TypeFall;
+
+    //private Collider area;
+    
 
     // Start is called before the first frame update
-    void Start()
-    {
     
-        //FallingTree = GetComponent<GameObject>();
-        area =  GetComponentInChildren<Collider>();
-        Destroy(this);
-        for(int i=0; i<50; i++){
-            x =  Random.Range( 100,  297);
-            y = -0.6F;
-            z =  Random.Range( 20,  855);
-            Instantiate(FallingTree, new Vector3(x,y,z), Quaternion.Euler(0,0,0));
-        }
-    }
 
     // Update is called once per frame
     void OnTriggerEnter(Collider collider){
-        if (collider.tag == "Player"){
+        TypeFall = Random.Range(1,5);
+            if (collider.tag == "Player"){
             GetComponent<Rigidbody>().isKinematic = false;
+            }
+            if (TypeFall == 1){
+                GetComponent<Rigidbody>().AddForce(Random.Range(25000,40000) * Time.deltaTime,0,0);
+
+            }
+            if (TypeFall == 2){
+                GetComponent<Rigidbody>().AddForce(Random.Range(-40000,-25000) * Time.deltaTime,0,0);
+
+            }
+            if (TypeFall == 3){
+                GetComponent<Rigidbody>().AddForce(0,0,Random.Range(25000,40000) * Time.deltaTime);
+
+            }
+            if (TypeFall == 4){
+                GetComponent<Rigidbody>().AddForce(0,0,Random.Range(-40000,-25000) * Time.deltaTime);
+
+            }
+            
             //GetComponent<Transform>().rotation = Quaternion.Euler(0,0,20);
-            GetComponent<Rigidbody>().AddForce(25000 * Time.deltaTime,0,0);
+            //  GetComponent<Rigidbody>().AddForce(25000 * Time.deltaTime,0,0);
         }
-    }
 }
+
