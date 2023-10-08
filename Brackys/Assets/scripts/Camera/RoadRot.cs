fb61f0cd325c2421e325e08c5817a32dff85bdaa
Onewaypie <Onewaypie@gmail.com>
2023-10-08 22:48:41 +0200

    got the road follow to work, kinda

--- Brackys/Assets/scripts/Camera/RoadRot.cs
+++ Brackys/Assets/scripts/Camera/RoadRot.cs
@@ -1,3 +1,4 @@
+
 using UnityEngine;
 
 
@@ -12,17 +13,21 @@ void Update() {
     Quaternion roadRotation = CalculateRoadRotation(cube.transform.position);
 
     // Apply the roadRotation to the cube
-    cube.transform.rotation = roadRotation;
+    
+    //cube.transform.rotation = Quaternion.Lerp(cube.transform.rotation,roadRotation,0.1f);         FOR full rotation
+    //cube.transform.rotation.y = Mathf.Lerp(cube.transform.rotation.y, roadRotation.y, 0.1f);      Doesnt work bruh
+
 
     // Add relative force to move the cube forward
     Rigidbody cubeRigidbody = cube.GetComponent<Rigidbody>();
-    cubeRigidbody.AddRelativeForce(Vector3.forward * speed); // Adjust speed as needed
+    //cubeRigidbody.AddRelativeForce(Vector3.forward * speed); // Adjust speed as needed
 }
 
 Quaternion CalculateRoadRotation(Vector3 position) {
     RaycastHit hit;
     if (Physics.Raycast(position, -Vector3.up, out hit)) {
         // Get the normal vector of the road at the hit point
+        
         Vector3 roadNormal = hit.normal;
 
         // Calculate the rotation that aligns the cube's up direction with the road's normal
