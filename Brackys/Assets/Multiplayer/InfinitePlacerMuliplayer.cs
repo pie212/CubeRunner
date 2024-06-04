4654e0d1eefed7e7158c3db669bc8a8d1dc45ccd
NoobinOut <bigboi@duck.com>
2024-06-04 16:41:36 -0400

    Made a start on multplayer

---
+++ Brackys/Assets/Multiplayer/InfinitePlacerMuliplayer.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InfinitePlacerMuliplayer : MonoBehaviour
+{
+
+    public GameObject[] Tiles;
+    Vector3 NextSpawnPoint;
+    public int AmountOfTiles = 10;
+    int i;
+
+    void PlaceTile()
+    {
+        GameObject temp = Instantiate(Tiles[Random.Range(0, Tiles.Length)], NextSpawnPoint, Quaternion.identity);
+        NextSpawnPoint = temp.transform.GetChild(0).transform.position;
+    }
+
+    private void Start()
+    {
+        while(i != AmountOfTiles)
+        {
+            PlaceTile();
+            i += 1;
+        }
+    }
+}
