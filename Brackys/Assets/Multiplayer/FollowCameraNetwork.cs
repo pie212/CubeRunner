d70df6f55482f891e524f0e91d8e4fa1676b04b1
NoobinOut <bigboi@duck.com>
2024-06-05 15:15:12 -0400

    Uga booga multiplayer go brrrrrr

---
+++ Brackys/Assets/Multiplayer/FollowCameraNetwork.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Netcode;
+using Unity.Networking;
+
+public class FollowCameraNetwork : NetworkBehaviour
+{
+
+    public override void OnNetworkSpawn()
+    {
+        if (!IsOwner) { Destroy(this); }
+    }
+
+    public Transform player;
+
+    public Vector3 offset;
+    public Vector3 offsetUPsideown;
+    public float smoothSpeed;
+    private Vector3 velocity = Vector3.zero;
+    private Vector3 smoothedPosition;
+    private Vector3 desiredPosition;
+    //public PLayerMovement movement;
+    // Update is called once per frame
+    void LateUpdate()
+    {
+
+        if (FindObjectOfType<PlayerNetworkedMovement>().gravity == 1) // if we are upside down
+        {
+            desiredPosition = player.position + offsetUPsideown;
+        }
+        if (FindObjectOfType<PlayerNetworkedMovement>().gravity == 0)
+        {
+            // Vector3 desiredPosition = player.position + offset;                                         //old system
+            // Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);  // old system
+
+            // transform.position = smoothedPosition;
+            desiredPosition = player.position + offset;
+        }
+
+        smoothedPosition.x = Mathf.SmoothDamp(transform.position.x, desiredPosition.x, ref velocity.x, 0.05f * Time.timeScale);
+        smoothedPosition.y = Mathf.SmoothDamp(transform.position.y, desiredPosition.y, ref velocity.y, 0.05f * Time.timeScale);
+        // smoothedPosition.z = Mathf.SmoothDamp(transform.position.z, desiredPosition.z, ref velocity.z, 0.01f* Time.timeScale);
+
+        // // Smoothly move the camera to the desired position using SmoothDamp
+        // transform.position = Vector3.SmoothDamp(transform.position, smoothedPosition, ref velocity, smoothSpeed);
+
+        transform.position = new Vector3(smoothedPosition.x, smoothedPosition.y, desiredPosition.z);
+    }
+}
